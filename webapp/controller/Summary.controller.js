253e04fac32d750f09f9275fc6003d89c5e11e67
Erick <simasgrilo86@gmail.com>
2024-06-18 17:00:03 -0300

    Implementation of "Summary" detail page

--- webapp/controller/Summary.controller.js
+++ webapp/controller/Summary.controller.js
@@ -1,18 +1,37 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller",
-        "sap/ui/model/json/JSONModel"
-    ],
-    function(BaseController, JSONModel) {
-      "use strict";
-  
-      return BaseController.extend("com.simasgrilo.homepage.homepageui5.controller.App", {
-        onInit: function() {
-          
-          const oModel = new JSONModel()
+	[
+		"sap/ui/core/mvc/Controller",
+		"sap/m/Text"
+	],
+	function (BaseController, Text) {
+		"use strict";
 
-        }
-      });
-    }
+		return BaseController.extend("com.simasgrilo.homepage.homepageui5.controller.Summary", {
+			onInit: function () {
+				const oModel = this.getOwnerComponent().getModel("summaryModel");
+				const aJson = oModel["oData"]["summary"];
+				const oView = this.getView().byId("summary");
+				var sContent = "";
+				for (let sText of aJson) {
+					sContent += sText + '\n\n'
+
+				}
+				var oText = new Text({
+					text: sContent + '\n',
+					renderWhitespace : true,
+					wrapping: true
+				});
+				oView.addContent(oText);
+				// aJson.forEach(function(elem) {
+				// 	this._appendDOM(elem, oArea);
+				// });
+			},
+
+
+			onClickMe : function () {
+				console.log(this.getView().getModel("summaryModel"));
+			}
+
+		});
+	}
   );
-  
