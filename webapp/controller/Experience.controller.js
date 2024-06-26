a0f8264a8eea8835c64f9db8db54eddfbb3895de
Erick <simasgrilo86@gmail.com>
2024-06-26 00:35:27 -0300

    Second version of "Experience session" - update view from controller.

--- webapp/controller/Experience.controller.js
+++ webapp/controller/Experience.controller.js
@@ -4,12 +4,33 @@ sap.ui.define([
     "sap/m/HBox",
     "sap/m/VBox",
     "sap/m/Title",
+    "sap/m/Panel"
     
-], function(Controller, Text){
+], function(Controller, Text, HBox, VBox, Title, Panel){
     "use strict";
     return Controller.extend("simasgrilo.github.io.controller.Experience", {
         onInit : function() {
-            
+            var oPage = this.getView().byId("experiencePage");
+            var oModel = this.getOwnerComponent().getModel("experienceModel");
+            var aData = oModel.getData()["Experience"];
+            for (let oData of aData) {
+                var oPanel = new Panel({
+                    headerText: oData["JobDescription"] + " @ "  + oData["Company"] + "\t" + oData["TimeFrame"],
+                    expandable: true
+                });
+                //var sContent = "".join(oData["Activities"]);
+                var sContent = "";
+                oData["Activities"].forEach(element => {
+                    sContent += element + "\n";
+                });
+                var oText = new Text({
+                    text : sContent,
+                    renderWhitespace: true
+                });
+                oText.addStyleClass("bullet-list");
+                oPanel.addContent(oText);
+                oPage.addContent(oPanel);
+            }
 
         }
     });
