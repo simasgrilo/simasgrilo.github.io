33c5bc59db38b37cdf3f5d1237295b1145a2a541
Erick <simasgrilo86@gmail.com>
2024-06-25 23:43:25 -0300

    First version of "Experience" section

---
+++ webapp/controller/Experience.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/Text",
+    "sap/m/HBox",
+    "sap/m/VBox",
+    "sap/m/Title",
+    
+], function(Controller, Text){
+    "use strict";
+    return Controller.extend("simasgrilo.github.io.controller.Experience", {
+        onInit : function() {
+            
+
+        }
+    });
+});
